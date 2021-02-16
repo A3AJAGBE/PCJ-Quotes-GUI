cac9e7ff6152c7ce72696872709f46d132c859be
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-16 08:00:08 +0000

    Add label

--- main.py
+++ main.py
@@ -15,18 +15,23 @@ interface = Tk()
 interface.title("A3AJAGBE QUOTE GUI")
 interface.config(padx=50, pady=50)
 
+pc_label = Label(text="Priyanka Chopra Says", font=("Tahoma", 20, "bold"), padx=10, pady=10)
+pc_label.grid(row=0, column=0)
+
 # Add quote background image
 canvas = Canvas(width=300, height=414)
 bg_img = PhotoImage(file="images/background.png")
 canvas.create_image(150, 207, image=bg_img)
-quote_text = canvas.create_text(150, 207, text="Priyanka Quote Goes HERE", width=250, font=("Tahoma", 30, "bold"),
+quote_text = canvas.create_text(150, 207, text="", width=250, font=("Tahoma", 30, "bold"),
                                 fill="#0066ff")
-canvas.grid(row=0, column=0)
+canvas.grid(row=1, column=0)
 
 # Add PCJ image to button
 pc_img = PhotoImage(file="images/pc.png")
 pc_button = Button(image=pc_img, highlightthickness=0, command=get_quote)
-pc_button.grid(row=1, column=0)
+pc_button.grid(row=2, column=0)
+
+get_quote()
 
 # Keep the interface open until exited
 interface.mainloop()
