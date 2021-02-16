6794e29272539a03670e2d211825f602c0300825
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-16 07:34:47 +0000

    Add interface setup

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+interface = Tk()
+interface.title("A3AJAGBE QUOTE GUI")
+interface.config(padx=50, pady=50)
+
+
+# Keep the interface open until exited
+interface.mainloop()
