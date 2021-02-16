1fdf9e3b04459f1982bc2dd19579e10ce82e0a46
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-16 07:37:35 +0000

    Add quote image

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ interface = Tk()
 interface.title("A3AJAGBE QUOTE GUI")
 interface.config(padx=50, pady=50)
 
+# Add quote background image
+canvas = Canvas(width=300, height=414)
+bg_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=bg_img)
+quote_text = canvas.create_text(150, 207, text="Priyanka Quote Goes HERE", width=250, font=("Tahoma", 30, "bold"),
+                                fill="white")
+canvas.grid(row=0, column=0)
+
 
 # Keep the interface open until exited
 interface.mainloop()
