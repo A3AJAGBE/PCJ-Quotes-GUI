8c0590cf53f1c5ae9d56c6851c1359e57c98080f
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-16 07:55:56 +0000

    Add get quote function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from tkinter import *
+import requests
+
+
+def get_quote():
+    """This function gets the quote from the api"""
+    response = requests.get("https://pc-quotes-api.herokuapp.com/quotes/v1.0/")
+    response.raise_for_status()
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
+
 
 interface = Tk()
 interface.title("A3AJAGBE QUOTE GUI")
@@ -6,15 +17,15 @@ interface.config(padx=50, pady=50)
 
 # Add quote background image
 canvas = Canvas(width=300, height=414)
-bg_img = PhotoImage(file="background.png")
+bg_img = PhotoImage(file="images/background.png")
 canvas.create_image(150, 207, image=bg_img)
 quote_text = canvas.create_text(150, 207, text="Priyanka Quote Goes HERE", width=250, font=("Tahoma", 30, "bold"),
-                                fill="white")
+                                fill="#0066ff")
 canvas.grid(row=0, column=0)
 
 # Add PCJ image to button
-pc_img = PhotoImage(file="pc.png")
-pc_button = Button(image=pc_img, highlightthickness=0)
+pc_img = PhotoImage(file="images/pc.png")
+pc_button = Button(image=pc_img, highlightthickness=0, command=get_quote)
 pc_button.grid(row=1, column=0)
 
 # Keep the interface open until exited
